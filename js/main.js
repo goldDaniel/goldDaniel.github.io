ac1ccbf1c1c021dbef5656a3fb3a2879f5281c67
Daniel Gold <gold.daniel@outlook.com>
2019-03-07 02:30:47 -0600

    transparent navbar added

--- js/main.js
+++ js/main.js
@@ -4,18 +4,18 @@ $(document).ready(function(){
     $("#portfolioBtn").click(function() {
         $([document.documentElement, document.body]).animate({
             scrollTop: $("#portfolio").offset().top - 50
-        }, 500);
+        }, 350);
     });
 
     $("#aboutBtn").click(function() {
         $([document.documentElement, document.body]).animate({
             scrollTop: $("#about").offset().top - 50
-        }, 500);
+        }, 350);
     });
 
     $("#contactBtn").click(function() {
         $([document.documentElement, document.body]).animate({
             scrollTop: $("#contact").offset().top - 50
-        }, 500);
+        }, 350);
     });
 }); 
