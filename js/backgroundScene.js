434b8768f7843a56d0b81a08bef44bea9c4aa5ef
Daniel Gold <gold.daniel@outlook.com>
2019-03-11 15:01:45 -0500

    reduced blocks drawn improving performance

--- js/backgroundScene.js
+++ js/backgroundScene.js
@@ -7,19 +7,19 @@ document.getElementById("threejs").appendChild( renderer.domElement );
 
 var cubes = [];
 
-for(var x = 0; x < 25; x++)
+for(var x = 0; x < 14; x++)
 {
-    for(var y = 0; y < 12; y++)
+    for(var y = 0; y < 10; y++)
     {
-        var geometry = new THREE.BoxGeometry(0.25, 0.25, 0.25);
+        var geometry = new THREE.BoxGeometry(0.4, 0.4, 0.4);
 
         var color = 0xffffff / 2 * x/10 + 0xffffff / 2 * y/10;
 
         var material = new THREE.MeshStandardMaterial( { color: color } );
         var cube = new THREE.Mesh( geometry, material );
 
-        cube.position.x = -8 + (x/30.0) * 20;
-        cube.position.y = -4 + (y/30.0) * 20;
+        cube.position.x = -8 + (x/25.0) * 30;
+        cube.position.y = -4 + (y/30.0) * 25;
 
         cubes.push(cube);
 
@@ -33,7 +33,7 @@ scene.add( ambient );
 var point = new THREE.PointLight( 0xeeeeee );
 point.position.x = 0;
 point.position.y = 1;
-point.position.z = 1;
+point.position.z = 12;
 scene.add( point );
 
 camera.position.z = 5;
@@ -47,8 +47,8 @@ var animate = function () {
 
     for(var i = 0; i < cubes.length; i++)
     {
-        cubes[i].rotation.x += 0.02;
-        cubes[i].rotation.y += 0.02;
+        cubes[i].rotation.x += 0.015;
+        cubes[i].rotation.y += 0.015;
     }
 
     renderer.render( scene, camera );
